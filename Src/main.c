cabe5ea0a1607c4776e0b4d1930d4a2c84521307
Geovani Junior <geovanilbrito@hotmail.com>
2019-05-28 00:07:11 +0100

    atualizacao

--- Src/main.c
+++ Src/main.c
@@ -23,7 +23,11 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+#include <stdio.h>
+#include "stm32f769i_discovery.h"
+#include "stm32f769i_discovery_lcd.h"
+#include "stm32f769i_discovery_ts.h"
+#include "stdbool.h"
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -33,7 +37,11 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define MAX_CONVERTED_VALUE   4095    // Max converted value
+#define AMBIENT_TEMP            25    // Ambient Temperature
+#define VSENS_AT_AMBIENT_TEMP  760    // VSENSE value (mv) at ambient temperature
+#define AVG_SLOPE               25    // Average_Solpe multiply by 10
+#define VREF				3300
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -42,6 +50,7 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+ADC_HandleTypeDef hadc1;
 
 DMA2D_HandleTypeDef hdma2d;
 
@@ -49,25 +58,75 @@ DSI_HandleTypeDef hdsi;
 
 LTDC_HandleTypeDef hltdc;
 
+TIM_HandleTypeDef htim6;
+TIM_HandleTypeDef htim7;
+
 SDRAM_HandleTypeDef hsdram1;
 
 /* USER CODE BEGIN PV */
-
+TS_StateTypeDef TS_State;
+uint16_t maxLCD = 0;
+volatile int flagT6 = 0;
+volatile int flagT7 = 0;
+volatile int counterT6 = 0;
+volatile int counterT7 = 0;
+volatile long int JTemp = 0; //Internal Temperature converted
+volatile uint32_t ConvertedValue; //Value to convert internal temperature
+char string[100];
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
+static void MX_ADC1_Init(void);
 static void MX_DMA2D_Init(void);
 static void MX_DSIHOST_DSI_Init(void);
 static void MX_FMC_Init(void);
 static void MX_LTDC_Init(void);
+static void MX_TIM6_Init(void);
+static void MX_TIM7_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void LCD_Config();
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
+
+
+
+void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* adcHandle)
+{
+	if(adcHandle == &hadc1)
+		ConvertedValue = HAL_ADC_GetValue(&hadc1);
+
+
+	}
+
+void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim)
+{
+	if(htim->Instance == TIM6)
+	{
+		counterT6++;
+		flagT6 = 1;
+	}
+
+	if(htim->Instance == TIM7)
+	{
+		counterT7++;
+		flagT7 = 1;
+	}
+}
+void gameboard()
+{
+	  for(int i = 0; i<=8; i++){
+		  BSP_LCD_DrawVLine(BSP_LCD_GetXSize()/40 + (BSP_LCD_GetXSize()/16)*i, BSP_LCD_GetYSize()/10, 400);
+	  }
+
+	  for(int j = 0; j<=8; j++){
+		  BSP_LCD_DrawHLine(BSP_LCD_GetXSize()/40, BSP_LCD_GetYSize()/10 + (BSP_LCD_GetYSize()/9.6)*j, 400);
+	  }
+}
 
 /* USER CODE END 0 */
 
@@ -78,7 +137,8 @@ static void MX_LTDC_Init(void);
 int main(void)
 {
   /* USER CODE BEGIN 1 */
-
+	TS_StateTypeDef TS_State;
+		char string[20];
   /* USER CODE END 1 */
   
 
@@ -106,18 +166,106 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
+  MX_ADC1_Init();
   MX_DMA2D_Init();
   MX_DSIHOST_DSI_Init();
   MX_FMC_Init();
   MX_LTDC_Init();
+  MX_TIM6_Init();
+  MX_TIM7_Init();
   /* USER CODE BEGIN 2 */
-
+  //initialization of the buttom
+    BSP_PB_Init(BUTTON_WAKEUP, BUTTON_MODE_GPIO);
+    //initialization of the LED's
+    BSP_LED_Init(LED_GREEN);
+    BSP_LED_Init(LED_RED);
+    //Configuration of the touchScreen
+    BSP_TS_Init(BSP_LCD_GetXSize(),BSP_LCD_GetYSize());
+    BSP_TS_ITConfig();
+    //Configuration of the LCD
+    LCD_Config();
+    //confugurations of the interrupt with adc1 for temperature data collection
+    HAL_ADC_Start_IT(&hadc1);
+    //configurations of the interrupt with timer 6 for total played time
+    HAL_TIM_Base_Start_IT(&htim6);
+    //configurations of the interrupt with timer 7 for temperature display
+    HAL_TIM_Base_Start_IT(&htim7);
+    //Initialization of the board
+    gameboard();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  while (1)
-  {
+    int init_tick_led1= HAL_GetTick();
+    int init_tick_led2= HAL_GetTick();
+
+    while (1)
+    {
+    		//---------------------------------LED----------------------------------------------------
+
+
+	  	  //LED blinks after 0.5 seconds
+	  	  if(HAL_GetTick() >= init_tick_led1 + 500)
+	  	  {
+	  		  init_tick_led1 = HAL_GetTick();
+	  		  BSP_LED_Toggle(LED1);
+	  	  }
+
+	  	  //LED blinks after 1 second (1000 ms)
+	  	  if(HAL_GetTick() >= init_tick_led2 + 1000)
+	  	  {
+	  		  init_tick_led2 = HAL_GetTick();
+	  		  BSP_LED_Toggle(LED2);
+	  	  }
+	  	//---------------------------------TEMPERATURA-------------------------------------------------
+
+
+	  	    	/*when  flagT7 is detected, it is set to 0 (so that the cicle restarts) convertedvalue gets the data
+	  	    	 * from the interruptadc1,then after converted the same way we did inthe class, the right value of the temperature is stored
+	  	    	 * in the variable JTEMP and then is displayed at the LCD upper rigth conner*/
+
+	  	    	if(flagT7)
+	  	    	{
+	  	    		flagT7 = 0;
+	  	    		ConvertedValue = HAL_ADC_GetValue(&hadc1);
+	  	    		JTemp = ((((ConvertedValue * VREF) / MAX_CONVERTED_VALUE) - VSENS_AT_AMBIENT_TEMP) * 10 / AVG_SLOPE) + AMBIENT_TEMP;
+	  	    		//Show temperature in celsius
+	  	    		sprintf(string, "Temp: %ld C", JTemp);
+	  	    		BSP_LCD_SetFont(&Font12);
+	  	    		BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize()/2 - 230, (uint8_t *)string, RIGHT_MODE);
+	  	    		BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+	  	    	}
+
+	  	// -----------------------------------tempo total-------------------------------------------------
+
+
+	  		  /*when  flagT6 is detected, it is set to 0 (so that the cicle restarts) and the value of the time
+	  		  *is printed to the string and then is displayed at the LCD upper rigth conner*/
+    	if(flagT6)
+    	{
+    		flagT6 = 0;
+    		//Show time in seconds
+    		sprintf(string, "Time: %d s", counterT6);
+    		BSP_LCD_SetFont(&Font12);
+    		BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize()/2 - 215, (uint8_t *)string, RIGHT_MODE);
+    		BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+    	}
+
+
+    		//-------------------------------TOUCHSCREEN-------------------------------------------
+
+    		/*when flag is detected, if the TS_State.touchDetected is diferrent than 0, it means that
+    		 * the touchscreen is being used and it prints the coordenates of theat point, given by
+    		 * TS_State.touchX[0] and TS_State.touchY[0] */
+    	BSP_TS_GetState(&TS_State);
+
+	  	  if(TS_State.touchDetected){
+	  		  sprintf(string, "X = %d", (int)TS_State.touchX[0]);
+	  		  BSP_LCD_DisplayStringAtLine(4, (uint8_t*)string);
+	  		  sprintf(string, "Y = %d", (int)TS_State.touchY[0]);
+	  		  BSP_LCD_DisplayStringAtLine(5, (uint8_t*)string);
+	  	  }
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -183,6 +331,68 @@ void SystemClock_Config(void)
   {
     Error_Handler();
   }
+}
+
+/**
+  * @brief ADC1 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC1_Init(void)
+{
+
+  /* USER CODE BEGIN ADC1_Init 0 */
+
+  /* USER CODE END ADC1_Init 0 */
+
+  ADC_AnalogWDGConfTypeDef AnalogWDGConfig = {0};
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC1_Init 1 */
+
+  /* USER CODE END ADC1_Init 1 */
+  /** Configure the global features of the ADC (Clock, Resolution, Data Alignment and number of conversion) 
+  */
+  hadc1.Instance = ADC1;
+  hadc1.Init.ClockPrescaler = ADC_CLOCK_SYNC_PCLK_DIV8;
+  hadc1.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc1.Init.ScanConvMode = DISABLE;
+  hadc1.Init.ContinuousConvMode = ENABLE;
+  hadc1.Init.DiscontinuousConvMode = DISABLE;
+  hadc1.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_NONE;
+  hadc1.Init.ExternalTrigConv = ADC_SOFTWARE_START;
+  hadc1.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc1.Init.NbrOfConversion = 1;
+  hadc1.Init.DMAContinuousRequests = DISABLE;
+  hadc1.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  if (HAL_ADC_Init(&hadc1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure the analog watchdog 
+  */
+  AnalogWDGConfig.WatchdogMode = ADC_ANALOGWATCHDOG_SINGLE_REG;
+  AnalogWDGConfig.HighThreshold = 0;
+  AnalogWDGConfig.LowThreshold = 0;
+  AnalogWDGConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  AnalogWDGConfig.ITMode = DISABLE;
+  if (HAL_ADC_AnalogWDGConfig(&hadc1, &AnalogWDGConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure for the selected ADC regular channel its corresponding rank in the sequencer and its sample time. 
+  */
+  sConfig.Channel = ADC_CHANNEL_TEMPSENSOR;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_56CYCLES;
+  if (HAL_ADC_ConfigChannel(&hadc1, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC1_Init 2 */
+
+  /* USER CODE END ADC1_Init 2 */
+
 }
 
 /**
@@ -415,6 +625,82 @@ static void MX_LTDC_Init(void)
 
 }
 
+/**
+  * @brief TIM6 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM6_Init(void)
+{
+
+  /* USER CODE BEGIN TIM6_Init 0 */
+
+  /* USER CODE END TIM6_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM6_Init 1 */
+
+  /* USER CODE END TIM6_Init 1 */
+  htim6.Instance = TIM6;
+  htim6.Init.Prescaler = 19999;
+  htim6.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim6.Init.Period = 9999;
+  htim6.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim6) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim6, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM6_Init 2 */
+
+  /* USER CODE END TIM6_Init 2 */
+
+}
+
+/**
+  * @brief TIM7 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM7_Init(void)
+{
+
+  /* USER CODE BEGIN TIM7_Init 0 */
+
+  /* USER CODE END TIM7_Init 0 */
+
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM7_Init 1 */
+
+  /* USER CODE END TIM7_Init 1 */
+  htim7.Instance = TIM7;
+  htim7.Init.Prescaler = 9999;
+  htim7.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim7.Init.Period = 9999;
+  htim7.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim7, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM7_Init 2 */
+
+  /* USER CODE END TIM7_Init 2 */
+
+}
+
 /* FMC initialization function */
 static void MX_FMC_Init(void)
 {
@@ -469,6 +755,7 @@ static void MX_FMC_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOE_CLK_ENABLE();
@@ -480,10 +767,67 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOJ_CLK_ENABLE();
 
+  /*Configure GPIO pin : PI13 */
+  GPIO_InitStruct.Pin = GPIO_PIN_13;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOI, &GPIO_InitStruct);
+
 }
 
 /* USER CODE BEGIN 4 */
 
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+	TS_StateTypeDef TS_State;
+	char string[10];
+
+	if(GPIO_Pin == GPIO_PIN_13)
+	{
+		BSP_TS_GetState(&TS_State);
+		BSP_LED_On(LED_RED);
+		BSP_LCD_SetTextColor(LCD_COLOR_BLUE);
+		sprintf(string, "X = %d",(int)TS_State.touchX[0]);
+		BSP_LCD_DisplayStringAt(0,4,(uint8_t*)string,0x03);
+		sprintf(string, "Y = %d",(int)TS_State.touchY[0]);
+		BSP_LCD_DisplayStringAt(0,40,(uint8_t*)string,0x03);
+		BSP_LCD_FillCircle(TS_State.touchX[0],TS_State.touchY[0],5);
+	}
+}
+
+static void LCD_Config(void)
+{
+  uint32_t  lcd_status = LCD_OK;
+
+  /* Initialize the LCD */
+  lcd_status = BSP_LCD_Init();
+  while(lcd_status != LCD_OK);
+
+  BSP_LCD_LayerDefaultInit(0, LCD_FB_START_ADDRESS);
+
+  /* Clear the LCD */
+  BSP_LCD_Clear(LCD_COLOR_WHITE);
+
+  /* Set LCD Example description */
+  BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+  BSP_LCD_SetFont(&Font12);
+  BSP_LCD_DisplayStringAt(0, BSP_LCD_GetYSize()- 20, (uint8_t *)"Copyright (c) Oak Tree Company", CENTER_MODE);
+  //STE THE COLOR OF THE
+  BSP_LCD_SetTextColor(LCD_COLOR_LIGHTRED);
+  BSP_LCD_FillRect(0, 0, BSP_LCD_GetXSize(), 45);
+  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_SetBackColor(LCD_COLOR_LIGHTRED);
+  BSP_LCD_SetFont(&Font24);
+  BSP_LCD_DisplayStringAt(0, 12, (uint8_t *)"WELCOME TO REVERSI", CENTER_MODE);
+
+  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_SetBackColor(LCD_COLOR_LIGHTRED);
+  BSP_LCD_SetFont(&Font24);
+
+  BSP_LCD_SetTextColor(LCD_COLOR_WHITE);
+  BSP_LCD_FillRect(BSP_LCD_GetXSize()/10, BSP_LCD_GetYSize()/10, 400, 400);
+  BSP_LCD_SetTextColor(LCD_COLOR_BLACK);
+}
 /* USER CODE END 4 */
 
 /**
